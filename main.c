29a4eb3d9e424588e99842a765e5510d5f44dedf
Rafaela Altheman <134940501+RafaAltheman@users.noreply.github.com>
2023-09-27 17:15:52 +0000

    menu

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "projfunc.h"
+
+int main(){
+    
+    while (1){
+        int opcao;
+        
+        printf("Bem-vindo ao menu do banco QuemPoupaTem: ");
+        printf("\n1. Novo Cliente\n");
+        printf("2. Apagar Cliente\n");
+        printf("3. Listar Clientes\n");
+        printf("4. Débito\n");
+        printf("5. Depósito\n");
+        printf("6. Extrato\n");
+        printf("7. Transfência entre contas\n");
+        printf("0. Sair\n");
+
+        printf("Escolha uma opção:");
+        scanf("%d", &opcao);
+
+        if (opcao==1){
+
+        } else if (opcao==2){
+
+        } else if (opcao==3){
+
+        } else if (opcao==4){
+
+        } else if (opcao==5){
+
+        } else if (opcao==6){
+
+        } else if (opcao==7){
+
+        } else if (opcao==8){
+
+        } else {
+            break;
+        }
+    }
+    return 0;
+}
